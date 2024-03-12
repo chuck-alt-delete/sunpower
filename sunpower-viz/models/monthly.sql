2433b695dcdcb67807b2ba7305c2a35079d53986
chuck-alt-delete <chuck@materialize.com>
2024-03-11 21:16:31 -0700

    more dashboards

--- sunpower-viz/models/monthly.sql
+++ sunpower-viz/models/monthly.sql
@@ -1,7 +1,8 @@
 select 
+    month(ts) as month_number,
     date_trunc('month', ts) as month,
     sum(production) as production,
     sum(consumption) as consumption,
     sum(grid) as grid
 from sunpower 
-group by month
+group by month_number, month
